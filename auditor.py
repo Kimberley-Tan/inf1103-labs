8005ec8b9faeeaa29261332235610db4832cc99e
Kimberley-Tan <2604076@sit.singaporetech.edu.sg>
2026-09-11 00:48:34 +0800

    Reject negative numbers

--- auditor.py
+++ auditor.py
@@ -2,7 +2,12 @@ print("This is a Smart Inventory Auditor. \nIt will continuously prompt for Stoc
 inventory = 0
 while True:
     stock = input("Enter Stock Quantity:")
+    strip_stock = stock.strip("-")
     if stock == "Quit" or stock == "quit":
         break
-    if stock.isdigit() == False:
+    if stock.isdigit() == False and strip_stock.isdigit() == False:
         print("Stock quantity is invalid please only enter numbers.")
+        continue
+    elif int(stock)<0:
+        print("Stock quantity is invalid please only enter positive numbers.")
+        continue
